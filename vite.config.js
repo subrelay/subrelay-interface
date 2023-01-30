5f997180dd42e481fdbe9a5f86d1627f84fae8a8
“Thi <thi@bespokify.com>
2023-01-30 11:28:13 +0700

    fix: add note

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       './runtimeConfig': './runtimeConfig.browser',
-      process: 'process/browser',
+      process: 'process/browser', // todo for bu xui
       stream: 'stream-browserify',
       zlib: 'browserify-zlib',
       util: 'util',
