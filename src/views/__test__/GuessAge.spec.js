44dade0979d2a3109e23c4a2b08995ddcf69abc8
To Hi <anhthichieu@gmail.com>
2023-05-18 20:55:45 +0700

    test: setup unit test

--- src/views/__test__/GuessAge.spec.js
+++ src/views/__test__/GuessAge.spec.js
@@ -1,6 +1,21 @@
 import { mount } from '@vue/test-utils';
 import GuessAge from '@/views/GuessAge.vue';
-// import { expect, test } from 'vitest';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+
+export const restHandlers = [
+  rest.get('https://api.agify.io/', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json([{ age: 55, name: 'tope' }]));
+  }),
+];
+const server = setupServer(...restHandlers);
+// Start server before all tests
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+//  Close server after all tests
+afterAll(() => server.close());
+// Reset handlers after each test `important for test isolation`
+afterEach(() => server.resetHandlers());
+
 const wrapper = mount(GuessAge, {
   props: { title: 'Guess User Age App' },
 });
@@ -16,4 +31,13 @@ it('Test if data is a function', () => {
 test('snapshot UI testing', () => {
   const wrapper = mount(GuessAge, {});
   expect(wrapper.text()).toMatchSnapshot();
+});
+
+test('has a button', () => {
+  expect(wrapper.find('button').exists()).toBe(true);
+});
+
+test('Button clicked', async () => {
+  const ac = await wrapper.get('button').trigger('click');
+  expect(wrapper.vm.search).toEqual('');
 });
--- vitest.config.js
+++ vitest.config.js
@@ -12,6 +12,7 @@ export default mergeConfig(
       exclude: [...configDefaults.exclude, 'e2e/*'],
       root: fileURLToPath(new URL('./', import.meta.url)),
       transformMode: { web: [/\.[jt]sx$/] },
+      coverage: { provider: 'istanbul' },
     },
   }),
 );
