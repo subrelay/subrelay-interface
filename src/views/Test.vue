010fab2b33ac44b42f3598cd3de349dd9eddabe4
To Hi <anhthichieu@gmail.com>
2023-05-18 22:10:41 +0700

    test: unit test

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,5 +1,6 @@
 <template>
   <guess-age />
+  <n-button @click="test">FOO</n-button>
 </template>
 
 <script setup>
@@ -7,6 +8,13 @@ import GuessAge from './GuessAge.vue';
 defineProps({
   title: String,
 });
+
+function test() {
+  var url = 'https://blog.logrocket.com/guide-vitest-automated-testing-vue-components/';
+  var windowName = 'NewWindow';
+  var windowFeatures = 'width=500,height=500';
+  window.open(url, windowName, windowFeatures);
+}
 </script>
 
 <script></script>
