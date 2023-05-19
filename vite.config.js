f8ddc9a53231aba1758e4f1b696d9f35fbb5d9af
To Hi <anhthichieu@gmail.com>
2023-05-19 09:44:31 +0700

    test: merge test config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 // import eslintPlugin from 'vite-plugin-eslint';
+import { fileURLToPath } from 'node:url';
+import { configDefaults } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -32,5 +34,14 @@ export default defineConfig({
 
   build: {
     chunkSizeWarningLimit: 2000,
+  },
+
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    exclude: [...configDefaults.exclude, 'e2e/*'],
+    root: fileURLToPath(new URL('./', import.meta.url)),
+    transformMode: { web: [/\.[jt]sx$/] },
+    coverage: { provider: 'istanbul' },
   },
 });
