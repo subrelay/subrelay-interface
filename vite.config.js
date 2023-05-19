c63997517ef7ec444be4451c4484b502b40aca40
To Hi <anhthichieu@gmail.com>
2023-05-19 09:51:10 +0700

    fix: test config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-// import eslintPlugin from 'vite-plugin-eslint';
-import { fileURLToPath } from 'node:url';
-import { configDefaults } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
