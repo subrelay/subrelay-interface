00acf5666cfdb4b98280be400d7210b792b8301a
To Hi <anhthichieu@gmail.com>
2023-06-16 16:23:05 +0700

    fix: test

--- cypress/e2e/editor.spec.js
+++ cypress/e2e/editor.spec.js
@@ -19,7 +19,7 @@ describe('Editor', () => {
       });
     });
 
-    it('TRIGGER - Can filter to select chain and event, and can add some filtering conditions', () => {
+    it.only('TRIGGER - Can filter to select chain and event, and can add some filtering conditions', () => {
       cy.getBySel('chain-dropdown').should('be.visible');
 
       // Display error if chain is not selected yet
