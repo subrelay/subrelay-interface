47ec85e445c3ffcaae4dea3e646301cc917f2deb
To Hi <anhthichieu@gmail.com>
2023-06-16 16:46:09 +0700

    fix: test

--- cypress/support/utils/initWorkflow.js
+++ cypress/support/utils/initWorkflow.js
@@ -6,12 +6,15 @@ export const initWorkflow = () => {
   // Select chain
   cy.getBySel('chain-dropdown').then((el) => {
     cy.wrap(el).type('dot').type('{enter}');
+    cy.wait('@getEvents');
   });
   cy.getBySel('chain-continue-btn').click();
 
   // Select event
   cy.getBySel('event-dropdown').then((el) => {
     cy.wrap(el).type('transfer').type('{enter}');
+    cy.wait('@getFilterFields');
+    cy.wait('@getCustomMsgFields');
   });
   cy.getBySel('event-continue-btn').click();
 
