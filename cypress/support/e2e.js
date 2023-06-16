e413cf2a4c3e070c60c5699e579df586db3ee0d1
To Hi <anhthichieu@gmail.com>
2023-06-16 11:26:43 +0700

    fix: test

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,15 +1,8 @@
 import './commands';
 
 Cypress.on('uncaught:exception', (err, runnable, promise) => {
-  const expectedErrors = ['ResizeObserver loop limit exceeded', 'Form validation failed'];
-  // return expectedErrors.some((expectedError) => err.message.includes(expectedError));
-
-  // return !err.message.includes('ResizeObserver loop limit exceeded') || !err.message.includes('Form validation failed');
-  // return err.message.includes('Form validation failed');
-
   if (promise) {
     return false;
   }
-
-  return !err.message.includes('ResizeObserver loop limit exceeded');
+  // return !err.message.includes('ResizeObserver loop limit exceeded');
 });
